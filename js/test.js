31ad91ef6fe351cf292e46852e3060a66c0356dd
JuanK Jocop <jcjocop@gmail.com>
2018-04-10 12:46:26 -0600

    Otro cambio mas en test.js

--- js/test.js
+++ js/test.js
@@ -3,3 +3,4 @@
 /* -------------------------------------------------------------------------------------------------------------------- */
 console.log("Por fin funcionó la integración de Git con VSCode ! Aleluya !!");
 console.log("Un segundo cambio");
+console.log("Tercer línea después del 4o. commit");
